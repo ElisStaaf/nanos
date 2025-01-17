11f6c1be2aff7523c77a01623b49c4aaf3279ffa
eliss <elis.staaf@proton.me>
2025-01-17 11:21:12 +0100

    dbg2

--- geniso.sh
+++ geniso.sh
@@ -1,12 +1,13 @@
 mkdir -p boot
-cp $LIMINE_PATH/limine-cd.bin boot || exit 1
-cp $LIMINE_PATH/limine-cd-efi.bin boot || exit 1
-cp $LIMINE_PATH/limine.sys boot || exit 1
-tar -c boot/usersp > boot/init.tar
 
-xorriso -as mkisofs -b limine-cd.bin \
+cp $LIMINE_PATH/limine-uefi-cd.bin boot || exit 1
+cp $LIMINE_PATH/limine-bios.sys boot || exit 1
+
+tar -c boot/usr > boot/init.tar
+
+xorriso -as mkisofs \
     -no-emul-boot -boot-load-size 4 -boot-info-table \
-    --efi-boot limine-cd-efi.bin \
+    --efi-boot limine-uefi-cd.bin \
     -efi-boot-part --efi-boot-image --protective-msdos-label \
     boot -o ${@:$#} > /dev/null 2>&1 || exit 1
 
